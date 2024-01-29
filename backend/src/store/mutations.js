c2057bcaa9b756bd80d021805406d7c6d91ddf82
khanhnguyenitq <nguyenkhanhnguyenk3@gmail.com>
2024-01-29 13:42:47 +0700

    Implement Products.vue component to show products

--- backend/src/store/mutations.js
+++ backend/src/store/mutations.js
@@ -1,12 +1,29 @@
 export function setUser(state, user) {
     state.user.data = user;
-  }
+}
 
-  export function setToken(state, token) {
+export function setToken(state, token) {
     state.user.token = token;
     if (token) {
-      sessionStorage.setItem('TOKEN', token);
+        sessionStorage.setItem("TOKEN", token);
     } else {
-      sessionStorage.removeItem('TOKEN')
+        sessionStorage.removeItem("TOKEN");
     }
-  }
+}
+
+export function setProducts(state, [loading, data = null]) {
+    if (data) {
+        console.log(data);
+        state.products = {
+            ...state.products,
+            data: data.data,
+            links: data.meta?.links,
+            page: data.meta.current_page,
+            limit: data.meta.per_page,
+            from: data.meta.from,
+            to: data.meta.to,
+            total: data.meta.total,
+        };
+    }
+    state.products.loading = loading;
+}
